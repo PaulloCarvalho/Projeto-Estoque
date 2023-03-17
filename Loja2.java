060ae76c71255f02c9a10594df546a3ec79d4d6f
Paullo Carvalho <106547997+PaulloCarvalho@users.noreply.github.com>
2023-03-17 00:37:12 -0300

    Create Loja2.java

---
+++ Loja2.java
@@ -0,0 +1,21 @@
+
+package Aplicacao;
+
+public class Prdutos {
+
+	public String produto;
+	public double quantidade;
+	public double preco;
+	
+	
+	public void addProdutos (int estoque) {
+		this.quantidade += estoque;
+	}
+		
+		public void subProdutos (int estoque) {
+			this.quantidade -= estoque;
+	
+		
+	}
+
+}
